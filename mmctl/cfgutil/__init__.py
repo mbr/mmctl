b83195f1a70f1f8b71d3b38e4c6e73cd17efd228
Marc Brinkmann <git@marcbrinkmann.de>
2011-11-09 12:50:47 +0100

    Finished configuration wizard.

--- mmctl/cfgutil/__init__.py
+++ mmctl/cfgutil/__init__.py
@@ -5,17 +5,45 @@ from flask import Blueprint, render_template, current_app, jsonify, request
 from ..utils import load_meta
 from .. import defaults
 
+import datetime
+import os
+
 import Ice
 
 cfgutil = Blueprint('cfgutil', __name__,
                     template_folder='templates')
 
-@cfgutil.route('/')
+@cfgutil.route('/', methods=('GET', 'POST',))
 def index():
-    form = {
-        'icestring': defaults.ICE_STRING,
-        'hostname': defaults.SERVER_HOSTNAME,
-    }
+    if 'POST' == request.method:
+        form = request.form
+
+        current_app.jinja_env.filters['repr'] = lambda v: repr(v)
+
+        # save configuration file
+        conffile = os.path.join(current_app.instance_path, 'mmctl.conf')
+        try:
+            with open(conffile, 'w') as c:
+                c.write(render_template('mmctl.conf',
+                    dt=datetime.datetime.now(),
+                    ice_string=form['icestring'],
+                    server_hostname=form['hostname'],
+                    mmctl_password_key=form['pbdk'],
+                    pbkdf2_salt=current_app.initial_salt,
+                    pbkdf2_iterations=current_app.initial_iterations,
+                    pbkdf2_keylength=current_app.initial_keylength
+                ))
+        except IOError:
+            return render_template('error.html',
+                                   conffile=conffile)
+
+        # display success message
+        return render_template('success.html')
+    else:
+        form = {
+            'icestring': defaults.ICE_STRING,
+            'hostname': defaults.SERVER_HOSTNAME,
+        }
     return render_template('index.html',
                            version=current_app.version,
                            form=form,
